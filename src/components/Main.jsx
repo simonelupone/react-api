5580521f11f2706fddfdb91ea7e66656543a4cea
simonelupone <simone.lupone@gmail.com>
2025-05-18 12:13:05 +0200

    feat: add actress endpoint

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,22 @@
+import {useState, useEffect} from "react";
+import axios from "axios";
+
+const actressEndpoint = 'https://lanciweb.github.io/demo/api/actresses/';
+
 const Main = () => {
+
+    // state actress variable
+    const [actress, setActress] = useState([])
+
+    // ajax function
+    const getActress = () => {
+        axios.get(actressEndpoint).then((response) => {
+            console.log(response.data)
+        })
+    }
+
+    useEffect(() => getActress, [])
+
     return (
         <main>
             <div className='w-full flex justify-center bg-blue-200'>
