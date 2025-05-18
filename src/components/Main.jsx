6f88259bfe62a8124c979490da1f207e3d4fc40b
simonelupone <simone.lupone@gmail.com>
2025-05-18 18:55:46 +0200

    feat: loop actors and actresses

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,17 +10,19 @@ const Main = () => {
     const [actresses, setActresses] = useState([])
     const [actors, setActors] = useState([])
 
+    const [cast, setCast] = useState([])
+
     // ajax function
     const getActress = () => {
         axios.get(actressesEndpoint).then((response) => {
-            // console.log(response.data)
+            console.log(response.data)
             setActresses(response.data)
         })
     }
 
     const getActors = () => {
         axios.get(actorsEndpoint).then((response) => {
-            // console.log(response.data)
+            console.log(response.data)
             setActors(response.data)
         })
     }
@@ -30,6 +32,12 @@ const Main = () => {
         getActors();
     }, [])
 
+    useEffect(() => {
+        if (actresses.length > 0 || actors.length > 0) {
+            setCast([...actresses, ...actors]);
+        }
+    }, [actresses, actors]);
+
     return (
         <main>
             <div className='w-full flex justify-center'>
@@ -37,30 +45,30 @@ const Main = () => {
                     <h2 className='text-3xl py-4'>Actresses</h2>
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                        {actresses.map((actress) => (
-                            <div key={`actress-${actress.id}`}
+                        {cast.map((c) => (
+                            <div key={`${c.name}-${c.id}`}
                                  className="max-w-sm rounded overflow-hidden shadow-lg hover:scale-105 hover:bg-neutral-200 cursor-pointer">
                                 <div className="h-64 w-full">
                                     <img
-                                        className="w-full h-full object-cover"
-                                        src={actress.image}
-                                        alt={actress.name}
+                                        className="w-full h-full object-cover object-center"
+                                        src={c.image}
+                                        alt={c.name}
                                     />
                                 </div>
                                 <div className="px-6 py-4">
-                                    <div className="font-bold text-xl mb-2">{actress.name}</div>
-                                    <p className="text-gray-700 text-base">{actress.biography}</p>
+                                    <div className="font-bold text-xl mb-2">{c.name}</div>
+                                    <p className="text-gray-700 text-base">{c.biography}</p>
                                 </div>
                                 <div className="px-6 pt-4 pb-2">
-                                    <div><span className='font-semibold'>Birthday:</span> {actress.birth_year}</div>
-                                    <div><span className='font-semibold'>Nationality:</span> {actress.nationality}</div>
-                                    <div><span className='font-semibold'>Awards:</span> {actress.awards}</div>
+                                    <div><span className='font-semibold'>Birthday:</span> {c.birth_year}</div>
+                                    <div><span className='font-semibold'>Nationality:</span> {c.nationality}</div>
+                                    <div><span className='font-semibold'>Awards:</span> {c.awards}</div>
                                 </div>
                             </div>
                         ))}
                     </div>
 
-                    <h2 className='text-3xl py-8'>Actors</h2>
+                    {/*<h2 className='text-3xl py-8'>Actors</h2>
 
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                         {actors.map((actor) => (
@@ -84,7 +92,7 @@ const Main = () => {
                                 </div>
                             </div>
                         ))}
-                    </div>
+                    </div>*/}
                 </div>
             </div>
         </main>
