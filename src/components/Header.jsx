f7e2c4836d83f1f18bae246f938c42499cf7372c
simonelupone <simone.lupone@gmail.com>
2025-05-18 17:51:53 +0200

    chore: minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header>
             <div className='w-full flex justify-center bg-neutral-200'>
-                <div className="w-3xl p-4">
+                <div className="w-7xl p-4">
                     <h1 className='text-5xl font-semibold'>Boolcast</h1>
                 </div>
             </div>
